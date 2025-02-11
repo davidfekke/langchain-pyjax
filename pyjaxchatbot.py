0c5f4eb27361f9f557b8b8057c88578396779dc8
David Fekke <david@fekke.com>
2025-02-10 19:57:23 -0500

    Adding model selection tool

--- pyjaxchatbot.py
+++ pyjaxchatbot.py
@@ -6,6 +6,9 @@ openai_api_key = os.getenv('OPENAI_API_KEY')
 
 st.title("💬 PyJax Chatbot")
 
+# Create a selection tool that will let the streamlit user choose the model they want to use between gpt-4.0 or o1-mini
+model = st.selectbox("Select a model", ["gpt-4o", "gpt-4o-mini", "o1-mini"])
+
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
@@ -21,7 +24,7 @@ if prompt := st.chat_input():
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
     # previous model was gpt-3.5-turbo
-    response = client.chat.completions.create(model="o1-mini", messages=st.session_state.messages)
+    response = client.chat.completions.create(model=model, messages=st.session_state.messages)
     msg = response.choices[0].message.content
     st.session_state.messages.append({"role": "assistant", "content": msg})
     st.chat_message("assistant").write(msg)
